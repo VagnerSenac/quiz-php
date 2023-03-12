f77a7f5f97ef390b44370d9c31151cb84cf91ee2
Vagner Lourenço <90052483+VagnerSenac@users.noreply.github.com>
2023-03-12 19:12:57 -0300

    Add files via upload

--- pergunta.php
+++ pergunta.php
@@ -9,28 +9,53 @@
 </head>
 <body>
     <main>
+    <?php
+        include "conexao.php";
+        $sqlBusca = "select * from t_quiz";
+        $todosOsQuiz = mysqli_query($conexao, $sqlBusca);
+
+        $total = "SELECT COUNT(*) as total FROM t_quiz";
+        $resultado = mysqli_query($conexao, $total);
+        $row = mysqli_fetch_assoc($resultado);
+        $total = $row['total'];
+
+        $umQuiz = mysqli_fetch_assoc($todosOsQuiz);
+        if (mysqli_num_rows($todosOsQuiz) > 0) {
+            // Inicializa a posição do registro
+            $posicao = 1;
+           
+        }
+
+        //while ($umQuiz = mysqli_fetch_assoc($todosOsQuiz)){
+
+            ?>
         <header class="titulo">
             <h1 class="quisp">QuisP</h1>
-            <h2 class="numero"> 1/10</h2>
+            <h2 class="numero"> <?php echo $posicao; ?>/<?php echo $total; ?></h2>
         </header>
+
         <div class="corpo">
-            <p class="pergunta">Como você sente sua pele?</p>
-            <img src="imagens/fotopergunta1.png" class="imagem-capa">
+            <p class="pergunta"><?php echo $umQuiz['pergunta']; ?></p>
+            <img src="<?php echo $umQuiz['imagem']; ?>" class="imagem-capa">
             <div class="alternativas">
                 <button>
-                   Muito Oleosa, de longe se ve brilho
+                <?php echo $umQuiz['alternativa1']; ?>
                 </button>
                 <button>
-                    Sinto que a testa e nariz são oleosos, já a bochecha é seca, chega a descascar
+                <?php echo $umQuiz['alternativa2']; ?>
                 </button>
                 <button>
-                    Não sinto minha pele nem seca e nem oleosa
+                <?php echo $umQuiz['alternativa3']; ?>
                 </button>
                 <button>
-                    Muito seca, chega a descascar
+                <?php echo $umQuiz['alternativa4']; ?>
                 </button>
             </div>
         </div>
+
+        <?php
+        //}
+        ?>
         <div class="rodapePrincipal">
             
         </div>
--- quiz-novo.php
+++ quiz-novo.php
@@ -6,7 +6,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <link rel="stylesheet" href="estilos.css">
   <title>Cadastro de Quiz</title>
 </head>
@@ -29,7 +30,7 @@
             <div class="col">
               <div class="mb-3">
                 <label class="form-label"><b>Alternativa 1:</b></label>
-                <input type="text" name="alternativa1"  class="form-control">
+                <input type="text" name="alternativa1" class="form-control">
               </div>
             </div>
           </div>
@@ -37,7 +38,7 @@
             <div class="col">
               <div class="mb-3">
                 <label class="form-label"><b>Alternativa 2:</b></label>
-                <input type="text" name="alternativa2"  class="form-control">
+                <input type="text" name="alternativa2" class="form-control">
               </div>
             </div>
           </div>
@@ -57,19 +58,34 @@
               </div>
             </div>
           </div>
-          <div class="row align-items-start">
-            <div class="col">
-              <div class="mb-3">
-                <label class="form-label"><b>Imagem:</b></label>
-                <input type="file" name="foto" />
-
-                <div class="container text-center">
-                </div>
-
-              </div>
-            </div>
+          <div class="form-floating">
+            <select name="fundo" class="form-select" id="imagem">
+              <option>Click e escolha</option>
+              <option value="imagens/fundo1.jpg" selected>Figura 1</option>
+              <option value="imagens/fundo2.jpg">Figura 2</option>
+              <option value="imagens/fundo3.jpg">Figura 3</option>
+              <option value="imagens/fundo4.jpg">Figura 4</option>
+              <option value="imagens/fundo5.jpg">Figura 5</option>
+              <option value="imagens/fundo6.jpg">Figura 6</option>
+            </select>
+            <label for="imagem">Selecione a imagem de tela</label>
           </div>
-          <button type="submit" class="btn btn-primary btn-lg">Cadastrar</button>
+          <hr  style="border:0">
+          <div class="card" style="background-color: #0D6374; border:0;">
+            <img id="imagemSelecionada" src="imagens/fundo1.jpg" alt="Imagem Selecionada">
+          </div>
+          <hr  style="border:0">
+          <script>
+            var select = document.getElementById("imagem");
+            var imagemSelecionada = document.getElementById("imagemSelecionada");
+
+            select.addEventListener("change", function () {
+              imagemSelecionada.src = select.value;
+            });
+          </script>
+          <div>
+            <button type="submit" class="btn btn-primary btn-lg">Cadastrar</button>
+          </div>
     </form>
 </body>
 
